adeac4f117995cbadd62836f0d54e55268302645
vasu-various <121292722+vasu-various@users.noreply.github.com>
2023-02-09 17:12:44 +0530

    Add files via upload

---
+++ Assignment/practice/age unenry oprator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter Your age :");
+	scanf("%d",&a);
+	
+	printf("\nYour age in next year is : %d",);
+	printf("\nYour age in previous year is : %d",);
+	
+}
