29bc24afdf11ff27ddf1c89b7a7343ccf3aaf570
vasu-various <121292722+vasu-various@users.noreply.github.com>
2023-04-14 17:18:16 +0530

    Add files via upload

---
+++ assessment/database/vasu1.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 14, 2023 at 01:47 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vasu1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) DEFAULT NULL,
+  `customer_name` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `grade` int(11) DEFAULT NULL,
+  `salesman_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `customer_name`, `city`, `grade`, `salesman_id`) VALUES
+(3002, 'Nick Rimando', 'New York', 100, 5001),
+(3007, 'Brad Davis', 'New York', 200, 5001),
+(3005, 'Graham Zusi', 'California', 200, 5002),
+(3008, 'Julian Green', 'London', 300, 5002),
+(3004, 'Fabian Johnson', 'Paris', 300, 5006),
+(3009, 'Geoff Cameron', 'Berlin', 100, 5003),
+(3003, 'Jozy Altidor', 'Moscow', 200, 5007),
+(3001, 'Brad Guzan', 'London', NULL, 5005);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `salesman`
+--
+
+CREATE TABLE `salesman` (
+  `salesman_id` int(11) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `commission` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `salesman`
+--
+
+INSERT INTO `salesman` (`salesman_id`, `name`, `city`, `commission`) VALUES
+(5001, 'James Hoog', 'New York ', 0.15),
+(5002, 'Nail Knite', 'Paris', 0.13),
+(5005, 'Pit Alex', 'London', 0.11),
+(5006, 'Mc Lyon', 'Paris', 0.14),
+(5007, 'Paul Adam', 'Rome', 0.13),
+(5003, 'Lauson Hen❘', 'San Jose ', 0.12);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
